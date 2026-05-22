7fca83945b06dfded43303b217fe3934941c8654
Eva Belanger <eva.belanger@epitech.eu>
2026-05-22 15:25:40 +0200

    <modify> (tardis_dashboard.py): box for result of prediction

--- tardis_dashboard.py
+++ tardis_dashboard.py
@@ -9,9 +9,9 @@ existing_departure_stations = df["Departure station"].unique()
 existing_arrival_stations = df["Arrival station"].unique()
 
 #Configuration of page
-st.title("Analyses des données de la SNCF")
-st.set_page_config("Analyses des données de la SNCF", page_icon=":bullettrain_side:", layout="wide",
-                   menu_items={'About': "Welcome to our interactive dashboard which allows you to predict future delays on a train journey."})
+st.title("🚉 Analyses des données de la SNCF")
+st.set_page_config("Tardis | Prévoir les retards SNCF", page_icon=":bullettrain_side:", layout="wide",
+                   menu_items={'About': "Bienvenue sur notre tableau de bord interactif qui vous permet de prévoir les retards futurs sur un trajet en train."})
 
 #Load of prediction's model
 @st.cache_resource
@@ -20,84 +20,31 @@ def load_model():
 
 model = load_model()
 
-# Correlation between trains late at departure and their departure station
-temp = df.groupby("Departure station").aggregate("sum").reset_index()
-temp['Correlation late / nb of trains'] = 100 * temp['Number of cancelled trains'] / temp['Number of real trains']
 
-tab1, tab2, tab3 = st.tabs(["Statistiques", "Graphiques", "Prédictions"])
+# Custom CSS for a better look
+st.markdown(
+    """
+    <style>
+    /* Adjusting metrics for theme compatibility */
+    [data-testid="stMetric"] {
+        background-color: rgba(151, 166, 195, 0.1);
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid rgba(151, 166, 195, 0.2);
+    }
+    .prediction-box {
+        background-color: rgba(2, 136, 209, 0.1);
+        padding: 20px;
+        border-radius: 15px;
+        border-left: 5px solid #0288d1;
+        margin-top: 20px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-# Pie chart of the cause of delays
-causes_cols = [
-    "Pct delay due to external causes",
-    "Pct delay due to infrastructure",
-    "Pct delay due to traffic management",
-    "Pct delay due to rolling stock",
-    "Pct delay due to station management and equipment reuse",
-    "Pct delay due to passenger handling"
-]
-causes_means = df[causes_cols].mean()
-
-# Rename the index
-causes_means.index = [
-    "Causes externes",
-    "Infrastructure",
-    "Gestion du trafic",
-    "Matériel roulant",
-    "Gestion en gare",
-    "Prise en charge voyageurs"
-]
-
-def convert_minutes(mins):
-    convert = {"hours": 0, "minutes": 0, "seconds": 0}
-    convert["hours"] = int(mins // 60)
-    convert["minutes"] = int(mins % 60)
-    convert["seconds"] = int((mins % 1) * 60)
-    return convert
-
-with tab1:
-    st.markdown("## Temps de trajet en fonction du service")
-    st.markdown("Avec:\n- Nationaux : les trains reliant deux gares françaises\n- Internationaux : les trains reliant une gare française et une gare internationnale.")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        avg_nat = df[df["Service"] == "National"]["Average journey time"].mean()
-        conv_nat = convert_minutes(avg_nat)
-        st.markdown(f"#### Temps moyen de voyage au national: {str(conv_nat["hours"]) + "h" if conv_nat["hours"] != 0 else ""}{conv_nat["minutes"]} min")
-    with col2:
-        avg_inter = df[df["Service"] == "International"]["Average journey time"].mean()
-        conv_inter = convert_minutes(avg_inter)
-        st.markdown(f"#### Temps moyen de voyage à l'international: {str(conv_inter["hours"]) + "h" if conv_inter["hours"] != 0 else ""}{conv_inter["minutes"]} min")
-    st.divider()
-
-    st.markdown("## Trains prévus et retardés")
-    st.markdown(f"#### Non-annulés: {100 * (1 - df["Number of cancelled trains"].sum() / df["Number of scheduled trains"].sum()):.2f} %")
-    col1, col2 = st.columns(2)
-    with col1:
-        late = convert_minutes(df["Average delay of late trains at departure"].mean())
-        st.markdown(f"#### Retard moyen au départ: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
-        delay = convert_minutes(df["Average delay of all trains at departure"].mean())
-        st.markdown(f"#### Temps moyen au départ: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
-    with col2:
-        late = convert_minutes(df["Average delay of late trains at arrival"].mean())
-        st.markdown(f"#### Retard moyen à l'arrivée: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
-        delay = convert_minutes(df["Average delay of all trains at arrival"].mean())
-        st.markdown(f"#### Temps moyen à l'arrivée: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
-
-with tab2:
-    st.subheader("Pourcentage de trains en retard au départ par rapport à la station")
-    st.bar_chart(temp, x="Departure station", y="Correlation late / nb of trains", height=400, x_label="Station de départ", y_label="Pourcentage")
-    st.divider()
-    st.subheader("Répartition des causes de retards")
-    col1, col2, col3 = st.columns([1, 3, 1])
-    with col2:
-        fig1, ax1 = plt.subplots(figsize=(8, 8))
-        colors = plt.cm.Paired.colors
-        ax1.pie(causes_means, labels=causes_means.index, autopct='%1.1f%%', startangle=90, colors=colors, textprops={'fontsize': 10})
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        st.pyplot(fig1, use_container_width=False)
-
-with tab3:
-    st.text("Example")
+result, stats, graphs, model_stats = st.tabs(["🔮 Résultat prédiction", "📊 Statistiques", "📈 Graphiques", "🖥️ Modèle de prédiction"])
 
 def get_values_dataset(df: pd.DataFrame, start, stop, month):
     """
@@ -122,6 +69,88 @@ def get_values_dataset(df: pd.DataFrame, start, stop, month):
     values["prev month"] = temp["Delay previous month"].mean()
     return values
 
+def convert_minutes(mins):
+    convert = {"hours": 0, "minutes": 0, "seconds": 0}
+    convert["hours"] = int(mins // 60)
+    convert["minutes"] = int(mins % 60)
+    convert["seconds"] = int((mins % 1) * 60)
+    return convert
+
+st.session_state.save_values = None
+
+with stats:
+    st.markdown("## Temps de trajet en fonction du service")
+    st.markdown("Avec :\n- Nationaux : les trains reliant deux gares françaises\n- Internationaux : les trains reliant une gare française et une gare internationnale.")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        avg_nat = df[df["Service"] == "National"]["Average journey time"].mean()
+        conv_nat = convert_minutes(avg_nat)
+        st.markdown(f"#### Temps moyen de voyage au national: {str(conv_nat["hours"]) + "h" if conv_nat["hours"] != 0 else ""}{conv_nat["minutes"]} min")
+    with col2:
+        avg_inter = df[df["Service"] == "International"]["Average journey time"].mean()
+        conv_inter = convert_minutes(avg_inter)
+        st.markdown(f"#### Temps moyen de voyage à l'international: {str(conv_inter["hours"]) + "h" if conv_inter["hours"] != 0 else ""}{conv_inter["minutes"]} min")
+    st.divider()
+
+    st.markdown("## Trains prévus et retardés")
+    st.markdown(f"#### Non-annulés: {100 * (1 - df["Number of cancelled trains"].sum() / df["Number of scheduled trains"].sum()):.2f} %")
+    col1, col2 = st.columns(2)
+    with col1:
+        late = convert_minutes(df["Average delay of late trains at departure"].mean())
+        st.markdown(f"#### Retards moyen au départ: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
+        delay = convert_minutes(df["Average delay of all trains at departure"].mean())
+        st.markdown(f"#### Temps moyen au départ: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
+    with col2:
+        late = convert_minutes(df["Average delay of late trains at arrival"].mean())
+        st.markdown(f"#### Retards moyen à l'arrivée: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
+        delay = convert_minutes(df["Average delay of all trains at arrival"].mean())
+        st.markdown(f"#### Temps moyen à l'arrivée: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
+    st.divider()
+
+    st.markdown("## Statistiques par stations")
+
+# Pie chart of the cause of delays
+causes_cols = [
+    "Pct delay due to external causes",
+    "Pct delay due to infrastructure",
+    "Pct delay due to traffic management",
+    "Pct delay due to rolling stock",
+    "Pct delay due to station management and equipment reuse",
+    "Pct delay due to passenger handling"
+]
+causes_means = df[causes_cols].mean()
+
+# Rename the index
+causes_means.index = [
+    "Causes externes",
+    "Infrastructure",
+    "Gestion du trafic",
+    "Matériel roulant",
+    "Gestion en gare",
+    "Prise en charge voyageurs"
+]
+
+# Correlation between trains late at departure and their departure station
+temp = df.groupby("Departure station").aggregate("sum").reset_index()
+temp['Correlation late / nb of trains'] = 100 * temp['Number of cancelled trains'] / temp['Number of real trains']
+
+with graphs:
+    st.subheader("Pourcentage de trains en retard au départ par rapport à la station")
+    st.bar_chart(temp, x="Departure station", y="Correlation late / nb of trains", height=400, x_label="Station de départ", y_label="Pourcentage")
+    st.divider()
+    st.subheader("Répartition des causes de retards")
+    col1, col2, col3 = st.columns([1, 3, 1])
+    with col2:
+        fig1, ax1 = plt.subplots(figsize=(8, 8))
+        colors = plt.cm.Paired.colors
+        ax1.pie(causes_means, labels=causes_means.index, autopct='%1.1f%%', startangle=90, colors=colors, textprops={'fontsize': 10})
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        st.pyplot(fig1, use_container_width=False)
+
+with model_stats:
+    st.text("Example")
+
 with st.sidebar:
     st.title("Prévisions des retards SNCF")
     st.selectbox("Station de départ", existing_departure_stations, key="departure_station", index=None, placeholder="Sélectionnez une station de départ")
@@ -131,10 +160,10 @@ with st.sidebar:
 
     if st.button("Afficher les prévisions de retards", type="primary") :
         if st.session_state.departure_station is None or st.session_state.arrival_station is None or st.session_state.departure_date is None or st.session_state.train_type is None:
-            st.warning("Valeur.s manquante.s")
+            st.warning("⚠️ Valeur.s manquante.s")
             st.stop()
         if st.session_state.departure_station == st.session_state.arrival_station:
-            st.error("Les stations de départ et d'arrivée ne peuvent pas être les mêmes.")
+            st.error("❌ Les stations de départ et d'arrivée ne peuvent pas être les mêmes.")
             st.stop()
         try:
             journey_date = st.session_state.departure_date
@@ -155,8 +184,32 @@ with st.sidebar:
                 'Number of trains delayed > 15min': [values["delayed"]],
                 'Delay previous month': [values["prev month"]]
             })
-            retard_estime = max(0, model.predict(donnees_entree)[0])
-            st.success(f"⏱️ Retard estimé : **{retard_estime:.0f} minutes**")
+            st.session_state.save_values = {
+                "late": max(0, model.predict(donnees_entree)[0]),
+                "start": departure,
+                "end": arrival,
+                "season": values["season"],
+                "date": journey_date
+            }
         except Exception as e:
             st.error("Erreur lors de la prédiction.")
             st.warning(f"Détail technique : {e}")
+    
+    st.divider()
+    st.info("Tableau de bord interactif qui permet de prévoir les futurs retards sur un trajet en train.")
+
+with result:
+    if (st.session_state.save_values):
+        tab = st.session_state.save_values
+        st.markdown(
+            f"""
+        <div class="prediction-box">
+            <h3>Résultat détaillé</h3>
+            <p><b>{tab["start"]}</b> -> <b>{tab["end"]}</b> - <b>{tab["date"].strftime("%d/%m/%Y")}</b> :</p>
+            <h3 style="color: #3182ce;">⏱️ Retard estimé : {tab["late"]:.0f} minutes</h3>
+        </div>
+        """,
+            unsafe_allow_html=True,
+        )
+    else:
+        st.info("👈 Remplissez les informations de votre prochain départ pour lancer une prédiction")
