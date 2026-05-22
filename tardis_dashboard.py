41f258e516728b78a60d7b19b8753919e84e1646
Eva Belanger <eva.belanger@epitech.eu>
2026-05-22 12:23:48 +0200

    <add> (tardis_dashboard.py): Statistics on delayed and cancelles trains

--- tardis_dashboard.py
+++ tardis_dashboard.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("cleaned_dataset.csv", sep=";")
 
 existing_departure_stations = df["Departure station"].unique()
 existing_arrival_stations = df["Arrival station"].unique()
 
+#Configuration of page
 st.title("Analyses des données de la SNCF")
 st.set_page_config("Analyses des données de la SNCF", page_icon=":bullettrain_side:", layout="wide",
                    menu_items={'About': "Welcome to our interactive dashboard which allows you to predict future delays on a train journey."})
 
-#Load of the prediction's model
+#Load of prediction's model
 @st.cache_resource
 def load_model():
     return joblib.load("model.joblib")
@@ -22,7 +24,7 @@ model = load_model()
 temp = df.groupby("Departure station").aggregate("sum").reset_index()
 temp['Correlation late / nb of trains'] = 100 * temp['Number of cancelled trains'] / temp['Number of real trains']
 
-tab1, tab2 = st.tabs(["Statistiques", "Prédictions"])
+tab1, tab2, tab3 = st.tabs(["Statistiques", "Graphiques", "Prédictions"])
 
 # Pie chart of the cause of delays
 causes_cols = [
@@ -45,26 +47,57 @@ causes_means.index = [
     "Prise en charge voyageurs"
 ]
 
+def convert_minutes(mins):
+    convert = {"hours": 0, "minutes": 0, "seconds": 0}
+    convert["hours"] = int(mins // 60)
+    convert["minutes"] = int(mins % 60)
+    convert["seconds"] = int((mins % 1) * 60)
+    return convert
+
 with tab1:
-    st.subheader("Pourcentage de trains en retard au départ par rapport à la station")
-    st.bar_chart(temp, x="Departure station", y="Correlation late / nb of trains", height=400, x_label="Station de départ", y_label="Pourcentage")
-    st.subheader("Répartition des causes de retards")
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(figsize=(8, 8))
-    colors = plt.cm.Paired.colors
-    ax.pie(causes_means, labels=causes_means.index, autopct='%1.1f%%', startangle=90, colors=colors, textprops={'fontsize': 10})
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig)
+    st.markdown("## Temps de trajet en fonction du service")
+    st.markdown("Avec:\n- Nationaux : les trains reliant deux gares françaises\n- Internationaux : les trains reliant une gare française et une gare internationnale.")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        avg_nat = df[df["Service"] == "National"]["Average journey time"].mean()
+        conv_nat = convert_minutes(avg_nat)
+        st.markdown(f"#### Temps moyen de voyage au national: {str(conv_nat["hours"]) + "h" if conv_nat["hours"] != 0 else ""}{conv_nat["minutes"]} min")
+    with col2:
+        avg_inter = df[df["Service"] == "International"]["Average journey time"].mean()
+        conv_inter = convert_minutes(avg_inter)
+        st.markdown(f"#### Temps moyen de voyage à l'international: {str(conv_inter["hours"]) + "h" if conv_inter["hours"] != 0 else ""}{conv_inter["minutes"]} min")
+    st.divider()
+
+    st.markdown("## Trains prévus et retardés")
+    st.markdown(f"#### Non-annulés: {100 * (1 - df["Number of cancelled trains"].sum() / df["Number of scheduled trains"].sum()):.2f} %")
+    col1, col2 = st.columns(2)
+    with col1:
+        late = convert_minutes(df["Average delay of late trains at departure"].mean())
+        st.markdown(f"#### Retard moyen au départ: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
+        delay = convert_minutes(df["Average delay of all trains at departure"].mean())
+        st.markdown(f"#### Temps moyen au départ: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
+    with col2:
+        late = convert_minutes(df["Average delay of late trains at arrival"].mean())
+        st.markdown(f"#### Retard moyen à l'arrivée: {str(late["hours"]) + "h" if late["hours"] != 0 else ""}{late["minutes"]} min")
+        delay = convert_minutes(df["Average delay of all trains at arrival"].mean())
+        st.markdown(f"#### Temps moyen à l'arrivée: {str(delay["hours"]) + "h" if delay["hours"] != 0 else ""}{delay["minutes"]} min")
 
 with tab2:
-    st.text("Example")
+    st.subheader("Pourcentage de trains en retard au départ par rapport à la station")
+    st.bar_chart(temp, x="Departure station", y="Correlation late / nb of trains", height=400, x_label="Station de départ", y_label="Pourcentage")
+    st.divider()
+    st.subheader("Répartition des causes de retards")
+    col1, col2, col3 = st.columns([1, 3, 1])
+    with col2:
+        fig1, ax1 = plt.subplots(figsize=(8, 8))
+        colors = plt.cm.Paired.colors
+        ax1.pie(causes_means, labels=causes_means.index, autopct='%1.1f%%', startangle=90, colors=colors, textprops={'fontsize': 10})
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        st.pyplot(fig1, use_container_width=False)
 
-def does_route_exists(df, start, end):
-    possibilities = df.groupby("Departure station").aggregate("sum").reset_index()
-    possibilities = possibilities[(possibilities["Departure station"] == start) & (possibilities["Arrival station"].str.contains(end))]
-    if len(possibilities) == 0:
-        return False
-    return True
+with tab3:
+    st.text("Example")
 
 def get_values_dataset(df: pd.DataFrame, start, stop, month):
     """
@@ -102,9 +135,6 @@ with st.sidebar:
             st.stop()
         if st.session_state.departure_station == st.session_state.arrival_station:
             st.error("Les stations de départ et d'arrivée ne peuvent pas être les mêmes.")
-            st.stop()
-        if not does_route_exists(df, st.session_state.departure_station, st.session_state.arrival_station):
-            st.info("Impossible de prédire le retard")
             st.stop()
         try:
             journey_date = st.session_state.departure_date
